2850d2bc123789f414572e40ddddc141906b10d8
radigin <VYRadygin@mephi.ru>
2018-01-09 16:39:41 +0300

    Небольшое исправление

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'virtual_lab/show_diagram'
-
+  get 'translations/current', to: 'virtual_lab#show_diagram'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
